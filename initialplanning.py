a394fdc5520490b08606d885f15061491244e1ee
Charmi Bhatt <charmi.bhatt2051@gmail.com>
2022-06-23 13:01:26 -0400

    add initial planning file

---
+++ initialplanning.py
@@ -0,0 +1,16 @@
+# A software package to generate rotational spectrum of molecules. (Astrochemistry, yay!)
+# Presumptions : Molecule is a symmterical top and is in Electric and vibrational ground state, 
+# so we only care about the transitions between rotational levels of that molecule. 
+
+# What this code does: 
+    # Inputs required: Temperature, Rotational constants (A,B or C), Jmax, Kmax
+
+    #define the above terms
+
+    #selection rules : make an array of allowed transitions
+
+    # Calculates frequencies/wavenumbers : uppper level energy - lower level energy
+
+        # write code to calculate energies of different levels and find wavenumber out of it
+
+    # calculate intensities : Honl-london factor * Boltzmann distribution
